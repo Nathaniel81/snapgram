dbb06b2ac0f040eb67d8a0cc603f6dbe1e82307e
Nathaniel81 <nathanield356@gmail.com>
2024-04-17 18:15:39 +0300

    Include creator field on Post serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -37,6 +37,7 @@ class PostSerializer(serializers.ModelSerializer):
 
         This method overrides the default to_representation method to include the file URL.
         """
+
         representation = super().to_representation(instance)
         if instance.file:
             # Add the file URL to the representation
