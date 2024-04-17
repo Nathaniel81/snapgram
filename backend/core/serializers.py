b74307d9ee009efce8fefad9e1fa184cf16582db
Nathaniel81 <nathanield356@gmail.com>
2024-04-17 11:41:18 +0300

    Updated PostSerializer to correctly handle file field

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,8 +1,43 @@
 from rest_framework import serializers
 from .models import Post
+from cloudinary.utils import cloudinary_url
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Post model.
+
+    This serializer handles serialization and deserialization of Post instances.
+    """
+
     class Meta:
         model = Post
-        fields = ['caption', 'file', 'location', 'tags']
+        exclude = ('creator',)
+
+    def create(self, validated_data):
+        """
+        Create a new Post instance.
+
+        This method overrides the default create method to handle file upload.
+
+        """
+
+        file = validated_data.pop('file', None)
+        post = super().create(validated_data)
+        if file:
+            post.file = file
+            post.save()
+        return post
+
+    def to_representation(self, instance):
+        """
+        Convert the Post instance to a representation.
+
+        This method overrides the default to_representation method to include the file URL.
+        """
+
+        representation = super().to_representation(instance)
+        if instance.file:
+            # Add the file URL to the representation
+            representation['file'] = cloudinary_url(instance.file.public_id)[0]
+        return representation
