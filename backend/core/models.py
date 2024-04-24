41957412b684963662259108978a90d86d42f259
Nathaniel81 <nathanield356@gmail.com>
2024-04-24 22:31:15 +0300

    Add ChatMessage and Message model

--- backend/core/models.py
+++ backend/core/models.py
@@ -16,3 +16,19 @@ class Post(models.Model):
 
     # def __str__(self):
     #     return self.creator.id
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255)
+    participants = models.ManyToManyField(User, related_name='chatrooms')
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_messages')
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
